b9d3043d08bce4629427afed13aaeca075583ffc
lrpanth90-eng <lrpanth90@gmail.com>
2026-01-01 01:02:23 +0530

    Update adminDashboard.js

--- js/admin/adminDashboard.js
+++ js/admin/adminDashboard.js
@@ -1,1 +1,22 @@
-// js/admin/adminDashboard.js
+import { db } from "../firebase/app.js";
+import { collection, getDocs } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const totalEl = document.getElementById("total");
+const activeEl = document.getElementById("active");
+const pendingEl = document.getElementById("pending");
+
+let total = 0, active = 0, pending = 0;
+
+const snap = await getDocs(collection(db, "batteries"));
+
+snap.forEach(doc => {
+  total++;
+  const d = doc.data();
+  if (d.activated) active++;
+  else pending++;
+});
+
+totalEl.innerText = total;
+activeEl.innerText = active;
+pendingEl.innerText = pending;
