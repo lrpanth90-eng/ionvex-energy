387dd239b1fad2100ca9c855e1359a7d1fe00b55
lrpanth90-eng <lrpanth90@gmail.com>
2026-01-01 01:21:07 +0530

    Implement addNewBattery function to add battery data

--- js/admin/batteryManager.js
+++ js/admin/batteryManager.js
@@ -1,1 +1,9 @@
-// js/admin/batteryManager.js
+import { addBattery } from "../firebase/firestore.js";
+
+export async function addNewBattery(serial, model) {
+  await addBattery(serial, {
+    model,
+    warrantyTill: "2028-12-31",
+    status: "inactive"
+  });
+}// js/admin/batteryManager.js
