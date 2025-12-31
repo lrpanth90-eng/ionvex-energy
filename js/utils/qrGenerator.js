2aa40eead329a7f8a8bde579071f1e1fb8ea3be2
lrpanth90-eng <lrpanth90@gmail.com>
2026-01-01 01:00:03 +0530

    Update qrGenerator.js

--- js/utils/qrGenerator.js
+++ js/utils/qrGenerator.js
@@ -1,1 +1,6 @@
-// js/utils/qrGenerator.js
+import QRCode from "https://cdn.jsdelivr.net/npm/qrcode@1.5.3/build/qrcode.min.js";
+
+export function generateQR(serial) {
+  const url = `https://ionvex-energy.vercel.app/customer/battery.html?serial=${serial}`;
+  QRCode.toCanvas(document.getElementById("qr"), url);
+}
