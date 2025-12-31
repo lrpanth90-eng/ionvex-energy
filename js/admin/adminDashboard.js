366ee223956d95e1981436277ec3dec4e76d98a6
lrpanth90-eng <lrpanth90@gmail.com>
2026-01-01 01:19:48 +0530

    Update adminDashboard.js

--- js/admin/adminDashboard.js
+++ js/admin/adminDashboard.js
@@ -1,22 +1,8 @@
-import { db } from "../firebase/app.js";
-import { collection, getDocs } from
-"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { protectPage } from "../firebase/auth.js";
 
-const totalEl = document.getElementById("total");
-const activeEl = document.getElementById("active");
-const pendingEl = document.getElementById("pending");
-
-let total = 0, active = 0, pending = 0;
-
-const snap = await getDocs(collection(db, "batteries"));
-
-snap.forEach(doc => {
-  total++;
-  const d = doc.data();
-  if (d.activated) active++;
-  else pending++;
+protectPage((user) => {
+  if (user.role !== "admin") {
+    alert("Access denied");
+    location.href = "/";
+  }
 });
-
-totalEl.innerText = total;
-activeEl.innerText = active;
-pendingEl.innerText = pending;
