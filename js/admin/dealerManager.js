d1b02a3c5a49698099c80e48f2685bdb11c15a30
lrpanth90-eng <lrpanth90@gmail.com>
2026-01-01 01:20:40 +0530

    Implement addDealer function to add new dealers

--- js/admin/dealerManager.js
+++ js/admin/dealerManager.js
@@ -1,1 +1,10 @@
-// js/admin/dealerManager.js
+import { db } from "../firebase/app.js";
+import { setDoc, doc } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
+export async function addDealer(uid, data) {
+  await setDoc(doc(db, "users", uid), {
+    ...data,
+    role: "dealer",
+    active: true
+  });
+}// js/admin/dealerManager.js
