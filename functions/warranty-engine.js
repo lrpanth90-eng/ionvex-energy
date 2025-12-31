df03a2a45eebda54088ab2d087b102d6260ea321
lrpanth90-eng <lrpanth90@gmail.com>
2026-01-01 01:35:09 +0530

    Update warranty-engine.js

--- functions/warranty-engine.js
+++ functions/warranty-engine.js
@@ -1,1 +1,22 @@
-// functions/warranty-engine.js
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+exports.warrantyEngine = async (batteryId, data) => {
+  const db = admin.firestore();
+
+  // RULE 1: Over temperature
+  if (data.temp > 60) {
+    await db.collection("batteries").doc(batteryId).update({
+      warrantyStatus: "VOID",
+      reason: "Overheat misuse"
+    });
+  }
+
+  // RULE 2: Deep discharge abuse
+  if (data.minVoltage < 2.5) {
+    await db.collection("batteries").doc(batteryId).update({
+      warrantyStatus: "VOID",
+      reason: "Cell deep discharge"
+    });
+  }
+};
