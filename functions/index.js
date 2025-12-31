25740dc6a44254efe5a394e001fe9267fd2da120
lrpanth90-eng <lrpanth90@gmail.com>
2026-01-01 01:37:30 +0530

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,14 @@
-exports.lockBattery = onCall(async (data) => {
-  await db.collection("batteries")
-    .doc(data.serial)
-    .update({ locked: true });
-});
+const { warrantyEngine } = require("./warranty-engine");
+const { autoLock } = require("./lock-engine");
+
+exports.onBatteryData = async (req, res) => {
+  const { batteryId, data } = req.body;
+
+  await warrantyEngine(batteryId, data);
+
+  if (data.theftDetected) {
+    await autoLock(batteryId, "THEFT");
+  }
+
+  res.send("Processed");
+};
